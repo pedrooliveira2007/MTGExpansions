99d78c09ee65647791b8d50739696eca7d167c02
Pedro <pedrooliveira2007@hotmail.com>
2019-12-05 10:32:00 +0000

    minor changes

--- app/src/main/java/com/lenoxys/mtgextensions/business/model/CardDetailFragment.java
+++ app/src/main/java/com/lenoxys/mtgextensions/business/model/CardDetailFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,18 +20,32 @@ public class CardDetailFragment extends Fragment {
 
     public CardDetailFragment() {
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_card_detail, container, false);
+
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        TextView cardName = view.findViewById(R.id.card_name);
+        TextView rarity = view.findViewById(R.id.card_rarity);
+        TextView manaCost = view.findViewById(R.id.card_manacost);
+        TextView artistName = view.findViewById(R.id.card_artist);
+        TextView price = view.findViewById(R.id.card_price);
+        TextView type = view.findViewById(R.id.card_type);
+        ImageView cardimage = view.findViewById(R.id.card_image);
+        Button backButton = view.findViewById(R.id.card_back);
     }
+
     @Override
     public void onResume() {
         super.onResume();
     }
+
     public static CardDetailFragment newInstance(String cardId) {
         CardDetailFragment fragment = new CardDetailFragment();
         Bundle args = new Bundle();
@@ -36,11 +53,14 @@ public class CardDetailFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
     private View.OnClickListener onItemClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
         }
     };
+
+
 }
 
 
