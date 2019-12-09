fb59e8056bce279c55ab865ead8fe5b311c1cda0
Pedro <pedrooliveira2007@hotmail.com>
2019-12-09 11:47:09 +0000

    older card fragment removed, added new one, layout changes, bug fixes

--- app/src/main/java/com/lenoxys/mtgextensions/ui/fragments/CardDetailFragment.java
+++ app/src/main/java/com/lenoxys/mtgextensions/ui/fragments/CardDetailFragment.java
@@ -25,7 +25,7 @@ public class CardDetailFragment extends Fragment implements View.OnClickListener
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_card_detail, container, false);
+        return inflater.inflate(R.layout.fragment_card_detail_firetrap, container, false);
     }
 
     @Override
